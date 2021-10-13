bb4301c582f40f4bf354466ddce2fb518c74f44f
zen <zenyui@gmail.com>
2021-10-13 03:40:26 -0400

    simplify

--- app/server/config.go
+++ app/server/config.go
@@ -1,12 +1,6 @@
 package server
 
 import (
-	"errors"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-
 	"github.com/caarlos0/env/v6"
 )
 
@@ -25,27 +19,6 @@ func NewConfigFromEnv() (*Config, error) {
 	opts := env.Options{RequiredIfNoDef: true}
 	if err := env.Parse(cfg, opts); err != nil {
 		return nil, err
-	}
-	// parse initial nodes
-	if i := os.Getenv("INITIAL_NODES"); i != "" {
-		cfg.InitialNodes = make(map[int]string)
-		for _, combo := range strings.Split(i, ",") {
-			values := strings.Split(combo, "=")
-			if len(values) != 2 {
-				errMsg := fmt.Sprintf("malformed initial nodes, %s", i)
-				return nil, errors.New(errMsg)
-			}
-			nodeID, err := strconv.Atoi(values[0])
-			if err != nil {
-				errMsg := fmt.Sprintf("malformed initial nodes, %s", i)
-				return nil, errors.New(errMsg)
-			}
-			nodeAddr := values[1]
-			cfg.InitialNodes[nodeID] = nodeAddr
-		}
-		for k, v := range cfg.InitialNodes {
-			fmt.Printf("found node %d at %s\n", k, v)
-		}
 	}
 	return cfg, nil
 }
