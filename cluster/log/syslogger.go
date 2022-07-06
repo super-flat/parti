1567bd092ae084bc82cd8909027c503b4b38e875
Kelvin Mungai <kelvin@mungai.me>
2022-07-06 19:20:14 -0400

    feat: add logger interface and system log wrapper for it

---
+++ cluster/log/syslogger.go
@@ -0,0 +1,61 @@
+package log
+
+import (
+	"log"
+)
+
+type wrapper struct {
+	*log.Logger
+}
+
+func (w *wrapper) Debug(args ...interface{}) {
+	w.Print(args...)
+}
+
+func (w *wrapper) Debugf(format string, args ...interface{}) {
+	w.Printf(format, args...)
+}
+
+func (w *wrapper) Info(args ...interface{}) {
+	w.Print(args...)
+}
+
+func (w *wrapper) Infof(format string, args ...interface{}) {
+	w.Printf(format, args...)
+}
+
+func (w *wrapper) Warn(args ...interface{}) {
+	w.Print(args...)
+}
+
+func (w *wrapper) Warnf(format string, args ...interface{}) {
+	w.Printf(format, args...)
+}
+
+func (w *wrapper) Error(args ...interface{}) {
+	w.Print(args...)
+}
+
+func (w *wrapper) Errorf(format string, args ...interface{}) {
+	w.Printf(format, args...)
+}
+
+func (w *wrapper) Fatal(args ...interface{}) {
+	w.Fatal(args...)
+}
+
+func (w *wrapper) Fatalf(format string, args ...interface{}) {
+	w.Fatalf(format, args...)
+}
+
+func (w *wrapper) Panic(args ...interface{}) {
+	w.Panic(args...)
+}
+
+func (w *wrapper) Panicf(format string, args ...interface{}) {
+	w.Panicf(format, args...)
+}
+
+func WrapSysLog(log *log.Logger) Logger {
+	return &wrapper{log}
+}
